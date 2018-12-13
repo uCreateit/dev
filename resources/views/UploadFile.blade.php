c9f15feefd459bfd6f37517e17e16703e5aa956e
Vikramjit Singh <vikramjitsingh@ucreate.co.in>
2018-12-13 15:25:08 +0530

    Fix Bucket spellings

--- resources/views/UploadFile.blade.php
+++ resources/views/UploadFile.blade.php
@@ -47,7 +47,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="bucket" class="col-md-4 col-form-label text-md-right">{{ __('Bukket Name ') }}</label>
+                            <label for="bucket" class="col-md-4 col-form-label text-md-right">{{ __('Bucket Name ') }}</label>
 
                             <div class="col-md-6">
                                 <input id="bucket" type="text" value="{{ old('bucket') }}" class="form-control{{ $errors->has('bucket') ? ' is-invalid' : '' }}" name="bucket" required>
